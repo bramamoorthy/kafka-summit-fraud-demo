4558218c7f48a5a8cec1121afc9779c6b2ad5305
Moxious <moxious@oldhat.org>
2019-07-12 11:53:57 -0400

    generic

--- cypher/run-all.sh
+++ cypher/run-all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+NEO4J_HOST=localhost
+NEO4J_PASSWORD=admin
+
 for file in `ls *.cypher` ; do
    echo $file
-   cat $file | cypher-shell -a localhost -u neo4j -p admin
+   cat $file | cypher-shell -a $NEO4J_HOST -u neo4j -p $NEO4J_PASSWORD
 done
