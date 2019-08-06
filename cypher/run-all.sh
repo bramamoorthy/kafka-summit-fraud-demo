49ed7db7e194b80499a8496679fc20cd25b36e34
Moxious <moxious@oldhat.org>
2019-08-06 07:55:21 -0400

    error handling

--- cypher/run-all.sh
+++ cypher/run-all.sh
@@ -6,6 +6,11 @@ NEO4J_PASSWORD=admin
 for file in `ls *.cypher` ; do
    echo $file
    cat $file | cypher-shell -a $NEO4J_HOST -u neo4j -p $NEO4J_PASSWORD
+
+   if [ $? -ne 0 ] ; then
+      echo "Bailing on detected error"
+      exit 1
+   fi
 done
 
 for N in 1 2 3 4 5 ; do
@@ -13,5 +18,10 @@ for N in 1 2 3 4 5 ; do
        echo "RUN $N - $file"
        export N
        cat $file | envsubst | cypher-shell -a $NEO4J_HOST -u neo4j -p $NEO4J_PASSWORD
+
+       if [ $? -ne 0 ] ; then
+          echo "Bailing on detected error"
+          exit 1
+       fi
     done
  done
