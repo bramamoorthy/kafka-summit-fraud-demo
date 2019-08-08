6f11e339f4c5b9d01f5369ed81b365c66b569b6d
johnymontana <lyonwj@gmail.com>
2019-08-08 11:23:00 -0600

    only show bloom embed, remove table

--- dashboard/ui/src/CaseList.js
+++ dashboard/ui/src/CaseList.js
@@ -14,7 +14,9 @@ import {
   Typography,
   TextField,
   Grid,
-  MenuItem
+  MenuItem,
+  Button,
+  Menu
 } from "@material-ui/core";
 
 import BloomEmbed from "./BloomEmbed";
@@ -37,6 +39,14 @@ const styles = theme => ({
   },
   container: {
     height: "100%"
+  },
+  formContainer: {
+    display: "grid",
+    gridTemplateColumns: "repeat(12, 1fr)",
+    gridGap: `${theme.spacing.unit * 3}px`
+  },
+  menuButton: {
+    minWidth: 200
   }
 });
 
@@ -44,24 +54,42 @@ class CaseList extends React.Component {
   constructor(props) {
     super(props);
 
-    this.BLOOM_BASE_URL =
-      process.env.REACT_APP_BLOOM_BASE_URL ||
-      "http://localhost:7474/browser/bloom/";
-
     this.state = {
       caseId: "",
       order: "asc",
       orderBy: "last_name",
       page: 0,
       rowsPerPage: 10,
-      usernameFilter: ""
+      usernameFilter: "",
+      anchorEl: null
     };
   }
 
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = event => {
+    console.log(event.target.bloomURL);
+
+    this.setState({ anchorEl: null });
+  };
+
   handleChange = name => event => {
-    this.setState({
-      [name]: event.target.value
-    });
+    console.log(name);
+    console.log(event);
+    console.log(event.nativeEvent.target.getAttribute("bloomurl"));
+
+    if (name === "caseId") {
+      this.setState({
+        caseId: event.target.value,
+        bloomURL: event.nativeEvent.target.getAttribute("bloomurl")
+      });
+    } else {
+      this.setState({
+        [name]: event.target.value
+      });
+    }
   };
 
   handleSortRequest = property => {
@@ -101,7 +129,10 @@ class CaseList extends React.Component {
         <Query
           query={gql`
             {
-              getAllCases
+              getAllCases {
+                caseId
+                bloomURL
+              }
             }
           `}
         >
@@ -111,183 +142,63 @@ class CaseList extends React.Component {
 
             return (
               <div className={this.props.classes.container}>
-                <TextField
-                  id="caselist-select"
-                  select
-                  label="Select Case"
-                  className={classes.textField}
-                  value={this.state.caseId}
-                  onChange={this.handleChange("caseId")}
-                  SelectProps={{
-                    MenuProps: {
-                      className: classes.menu
-                    }
-                  }}
-                  helperText="Please select a case"
-                  margin="normal"
-                  variant="outlined"
-                >
-                  {data.getAllCases.map(option => (
-                    <MenuItem key={option} value={option}>
-                      {option}
-                    </MenuItem>
-                  ))}
-                </TextField>
-                <Query
-                  query={gql`
-                    query usersPaginateQuery(
-                      $first: Int
-                      $offset: Int
-                      $caseId: String
-                      $orderBy: [_PartyOrdering]
-                      $filter: _PartyFilter
-                    ) {
-                      Party(
-                        case_id: $caseId
-                        first: $first
-                        offset: $offset
-                        orderBy: $orderBy
-                        filter: $filter
-                      ) {
-                        last_name
-                        first_name
-                        louvainCommunity
-                        occupation
-                        sharedIdentitySize
-                        bloomURL
+                <form className={classes.formContainer}>
+                  <TextField
+                    id="caselist-select"
+                    select
+                    label="Select Case"
+                    className={classes.textField}
+                    value={this.state.caseId}
+                    onChange={this.handleChange("caseId")}
+                    SelectProps={{
+                      MenuProps: {
+                        className: classes.menu
                       }
-                    }
-                  `}
-                  variables={{
-                    first: this.state.rowsPerPage,
-                    offset: this.state.rowsPerPage * this.state.page,
-                    orderBy: this.state.orderBy + "_" + this.state.order,
-                    caseId: this.state.caseId,
-                    filter: this.getFilter()
-                  }}
-                >
-                  {({ loading, error, data }) => {
-                    if (loading) return <p>Loading...</p>;
-                    if (error) return <p>Error</p>;
-
-                    return (
-                      <Grid
-                        container
-                        spacing={2}
-                        className={this.props.classes.container}
+                    }}
+                    helperText="Please select a case"
+                    margin="normal"
+                    variant="outlined"
+                  >
+                    {data.getAllCases.map(option => (
+                      <MenuItem
+                        key={option.caseId}
+                        value={option.caseId}
+                        bloomurl={option.bloomURL}
                       >
-                        <Grid item xs={9}>
-                          <BloomEmbed
-                            bloomURL={`${this.BLOOM_BASE_URL}?perspective=Fraud&search=View+Case+${this.state.caseId}`}
-                          />
-                        </Grid>
-                        <Grid item xs={3}>
-                          <Table className={this.props.classes.table}>
-                            <TableHead>
-                              <TableRow>
-                                <TableCell
-                                  key="last_name"
-                                  sortDirection={
-                                    orderBy === "last_name" ? order : false
-                                  }
-                                >
-                                  <Tooltip
-                                    title="Sort"
-                                    placement="bottom-start"
-                                    enterDelay={300}
-                                  >
-                                    <TableSortLabel
-                                      active={orderBy === "last_name"}
-                                      direction={order}
-                                      onClick={() =>
-                                        this.handleSortRequest("last_name")
-                                      }
-                                    >
-                                      Last Name
-                                    </TableSortLabel>
-                                  </Tooltip>
-                                </TableCell>
-                                <TableCell
-                                  key="louvainCommunity"
-                                  sortDirection={
-                                    orderBy === "louvainCommunity"
-                                      ? order
-                                      : false
-                                  }
-                                  numeric
-                                >
-                                  <Tooltip
-                                    title="Sort"
-                                    placement="bottom-end"
-                                    enterDelay={300}
-                                  >
-                                    <TableSortLabel
-                                      active={orderBy === "louvainCommunity"}
-                                      direction={order}
-                                      onClick={() =>
-                                        this.handleSortRequest(
-                                          "louvainCommunity"
-                                        )
-                                      }
-                                    >
-                                      Louvain Community
-                                    </TableSortLabel>
-                                  </Tooltip>
-                                </TableCell>
-                                <TableCell
-                                  key="sharedIdentitySize"
-                                  sortDirection={
-                                    orderBy === "sharedIdentitySize"
-                                      ? order
-                                      : false
-                                  }
-                                  numeric
-                                >
-                                  <Tooltip
-                                    title="Sort"
-                                    placement="bottom-start"
-                                    enterDelay={300}
-                                  >
-                                    <TableSortLabel
-                                      active={orderBy === "sharedIdentitySize"}
-                                      direction={order}
-                                      onClick={() =>
-                                        this.handleSortRequest(
-                                          "sharedIdentitySize"
-                                        )
-                                      }
-                                    >
-                                      Shared Identities
-                                    </TableSortLabel>
-                                  </Tooltip>
-                                </TableCell>
-                              </TableRow>
-                            </TableHead>
-                            <TableBody>
-                              {data.Party.map(n => {
-                                return (
-                                  <TableRow key={n.last_name}>
-                                    <TableCell component="th" scope="row">
-                                      <a href={n.bloomURL} target="_blank">
-                                        {n.first_name + " " + n.last_name}
-                                      </a>
-                                    </TableCell>
-                                    <TableCell numeric>
-                                      {n.louvainCommunity}
-                                    </TableCell>
-                                    <TableCell numeric>
-                                      {n.sharedIdentitySize}
-                                    </TableCell>
-                                  </TableRow>
-                                );
-                              })}
-                            </TableBody>
-                          </Table>
-                        </Grid>
-                      </Grid>
-                    );
-                  }}
-                </Query>
+                        {option.caseId}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                  <div>
+                    <Button
+                      aria-controls="simple-menu"
+                      aria-haspopup="true"
+                      onClick={this.handleClick}
+                      className={classes.menuButton}
+                    >
+                      Adjudicate Case
+                    </Button>
+                    <Menu
+                      id="simple-menu"
+                      anchorEl={this.state.anchorEl}
+                      keepMounted
+                      open={Boolean(this.state.anchorEl)}
+                      onClose={this.handleClose}
+                    >
+                      <MenuItem onClick={this.handleClose}>Fraud</MenuItem>
+                      <MenuItem onClick={this.handleClose}>Not Fraud</MenuItem>
+                    </Menu>
+                  </div>
+                </form>
+                <Grid
+                  container
+                  spacing={2}
+                  className={this.props.classes.container}
+                >
+                  <Grid item xs={12}>
+                    <BloomEmbed bloomURL={this.state.bloomURL} />
+                  </Grid>
+                </Grid>
               </div>
             );
           }}
